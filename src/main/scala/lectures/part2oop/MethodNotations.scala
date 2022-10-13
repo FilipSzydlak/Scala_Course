645415c000dcfee2da4f251b403145a5a150eeeb
Filip Szydłak <266912@student.pwr.edu.pl>
2022-10-13 20:22:16 +0200

    Method Notations Exercises

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -2,29 +2,41 @@ package lectures.part2oop
 
 import scala.language.postfixOps
 
-object MethodNotations extends App{
+object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String){
+  class Person(val name: String, favoriteMovie: String, val age : Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
-    def +(person:Person): String = s"${this.name} is hanging out with ${person.name}"
+
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+
     def unary_! : String = s"$name" // space between ! and  : is important
+
     def isAlive: Boolean = true
+
     def apply(): String = s"Hi, my name is $name and my favorite movie is $favoriteMovie"
+    def apply(n : Int) : String = s"$name watched $favoriteMovie $n times"
+
+    def learns(subject : String) : String = s"$name learns $subject"
+    def learnsScala : String = this learns "Scala"
   }
 
   val mary = new Person("Mary", "Inception")
   println(mary.likes("Inception"))
-  println(mary likes "Inception")// equivalent
+  println(mary likes "Inception") // equivalent
   // infix notation = operator notation ONLY WHEN METHOD HAS SINGLE PARAMETER (SYNTACTIC SUGAR)
 
   //"operators" i
   // n Scala
   val tom = new Person("Tom", "Fight Club")
   println(mary + tom) // hangOutWith works as "OPERATOR" (IT IS POSSIBLE TO RENAME IT TO +)
-  println(mary.+ (tom))
+  println(mary.+(tom))
 
-  println(1 + 2)//OPERATORS ARE METHODS IN SCALA
-  println(1.+ (2))
+  println(1 + 2) //OPERATORS ARE METHODS IN SCALA
+  println(1.+(2))
 
   // ALL OPERATORS ARE METHODS in SCALA
   // Akka actors have ! ?
@@ -39,13 +51,34 @@ object MethodNotations extends App{
   println(mary.unary_!)
 
 
-  //POSTFIX notation [FUNCTION DOES NOT RECEIVE ANY PARAMETERS
+  //POSTFIX notation [FUNCTION DOES NOT RECEIVE ANY PARAMETERS]
   println(mary.isAlive)
   println(mary isAlive)
 
   // apply
   println(mary.apply())
-  println(mary())// equivalent
+  println(mary()) // equivalent
 
+  /*
+  1. OVERLOAD the + operator
+  mary + "the rocktars" => new person "Mary (the rockstar)"
+
+  2. Add an age to the Persom class
+  Add a unary + operator => new person with the age + 1
+  +mary => mary with the age incrementer
+
+  ++
+  3. Add a "learns" method in the Person class => "Mary learns Scala"
+     Add a learnsScala method, calls learns method with "Scala".
+    Use it in postfix notation.
+
+  4. Overload the apply method
+    mary.apply(2) => "Mary watched Inception 2 times"
+  */
+
+  println((mary + "The Rockstar")())
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(10))
 
 }
